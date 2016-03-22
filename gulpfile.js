9499fafa88d5de7879d3693592a5505a92512528
Sam Morrison <sam@sammorrison.net>
2016-03-21 22:05:01 -0400

    base content commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	minifycss = require('gulp-clean-css'),
+	imagemin = require('gulp-imagemin'),
+	htmlreplace = require('gulp-html-replace'),
+	copy = require('gulp-copy'),
+	webserver = require('gulp-webserver'),
+	del = require('del');
+
+gulp.task('default', ['del', 'minify-js', 'minify-css', 'imagemin', 'html-replace', 'copy-icon']);
+
+gulp.task('dev', function () {
+	gulp.start('webserver');
+});
+
+gulp.task('del', function (cb) {
+	del(['dist/**', '!dist'], cb);
+});
+
+gulp.task('html-replace', ['del'], function () {
+	return gulp.src('index.html')
+		.pipe(htmlreplace({ js: 'js/main.min.js', css: 'css/main.min.css' }))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-js', ['del'], function () {
+	return gulp.src('js/**/*')
+		.pipe(concat('main.js'))
+		.pipe(uglify({ mangle: false }))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minify-css', ['del'], function () {
+	return gulp.src('css/*.css')
+		.pipe(concat('main.css'))
+		.pipe(minifycss())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('imagemin', ['del'], function () {
+	return gulp.src('images/*')
+		.pipe(imagemin({progressive: true}))
+		.pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('copy-icon', ['del'], function () {
+	return gulp.src('favicon.ico')
+		.pipe(copy('dist', { prefix: 1 }));
+});
+
+gulp.task('webserver', function() {
+	gulp.src('/')
+		.pipe(webserver({
+			livereload: true,
+			open: true
+		}));
+});
