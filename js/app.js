6e97037c14e5550c8d2e3a4a12337c0f41235555
Sam Morrison <sam@sammorrison.net>
2016-04-11 23:58:42 -0400

    remove routes

--- js/app.js
+++ js/app.js
@@ -1,25 +1,5 @@
-var app = angular.module('portfolio', ['ngRoute']);
+var app = angular.module('portfolio', []);
 
-app.config(function($routeProvider, $locationProvider) {
-	$routeProvider
-		.when('/', {
-		templateUrl : 'pages/home.html',
-		})
-		.when('/ombuds', {
-		templateUrl : 'pages/ombuds.html',
-		})
-		.when('/weatrit', {
-		templateUrl : 'pages/weatrit.html',
-		})
-		.when('/crr', {
-		templateUrl : 'pages/crr.html',
-		})
-		.when('/spectrum', {
-		templateUrl : 'pages/spectrum.html',
-		})
-		.when('/leafco', {
-		templateUrl : 'pages/leafco.html',
-		});
-
+app.config(function($locationProvider) {
 	$locationProvider.html5Mode(true);
 });
---
+++ js/controllers/ProjectCtrl.js
@@ -0,0 +1,5 @@
+app.controller('ProjectCtrl', function ($scope, $http, $location) {
+	this.openProject = function () {
+		console.log('hi');
+	}
+});
