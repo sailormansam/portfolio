784d423b4972c9024dc729545d53a5227ac95ca3
Sam Morrison <sam@sammorrison.net>
2016-05-26 23:59:41 -0400

    next and previous navigation

--- js/controllers/ProjectCtrl.js
+++ js/controllers/ProjectCtrl.js
@@ -1,6 +1,19 @@
 app.controller('ProjectCtrl', ['$location', function ($location) {
+	this.projects = [
+		'impact',
+		'leafco',
+		'ombuds',
+		'spectrum',
+		'women-in-engineering'
+	];
 	this.project = $location.path().substring(1);
 	this.location = 'pages' + $location.path() + '.html';
 
 	this.absUrl = $location.absUrl();
+
+	var currentIndex = this.projects.indexOf(this.project);
+
+	// set the previous and next projects to navigate to
+	this.next = this.project[(currentIndex + 1 > this.projects.length) ? 0 : currentIndex + 1];
+	this.prev = this.project[(currentIndex - 1 < 0) ? this.projects.length : currentIndex - 1];
 }]);
