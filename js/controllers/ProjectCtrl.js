2c776f7cd6c60de58e97cfd80d8588eeffe7c09d
Sam Morrison <sam@sammorrison.net>
2016-05-28 22:36:55 -0400

    names for project detail nav updated

--- js/controllers/ProjectCtrl.js
+++ js/controllers/ProjectCtrl.js
@@ -6,6 +6,15 @@ app.controller('ProjectCtrl', ['$location', function ($location) {
 		'spectrum',
 		'women-in-engineering'
 	];
+
+	this.names = [
+		'Impact',
+		'Leaf Co.',
+		'Ombuds',
+		'Spectrum',
+		'Women in Engineering'
+	];
+
 	this.project = $location.path().substring(1);
 	this.location = 'pages' + $location.path() + '.html';
 
@@ -14,6 +23,12 @@ app.controller('ProjectCtrl', ['$location', function ($location) {
 	var currentIndex = this.projects.indexOf(this.project);
 
 	// set the previous and next projects to navigate to
-	this.next = this.projects[(currentIndex + 1 >= this.projects.length) ? 0 : currentIndex + 1];
-	this.prev = this.projects[(currentIndex - 1 < 0) ? this.projects.length - 1 : currentIndex - 1];
+	var nextIndex = (currentIndex + 1 >= this.projects.length) ? 0 : currentIndex + 1;
+	var prevIndex = (currentIndex - 1 < 0) ? this.projects.length - 1 : currentIndex - 1;
+
+	this.next = this.projects[nextIndex];
+	this.prev = this.projects[prevIndex];
+
+	this.nextTitle = this.names[nextIndex];
+	this.prevTitle = this.names[prevIndex];
 }]);
